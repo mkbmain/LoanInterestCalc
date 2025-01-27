bff618c00fbc3cc25b4f8d485ec2ffa94915ea30
mkbmain <michael.bourke@live.co.uk>
2025-01-27 16:50:45 +0000

    Interest Calc

---
+++ InterestCalcCSharp/Program.cs
@@ -0,0 +1,77 @@
+﻿const int amount = 200_000;
+const double interestRate = 4.15;
+
+Console.WriteLine(
+    $"Years,PaymentAmount,PrincipalAmount,InterestAmount,RemainingBalance,YearTotal, 3 year total,5 year total, 10 year total");
+for (int year = 5; year <= 30; year += 5)
+{
+    var paymentSchedule = CalculateAmortizationSchedule(new LoanRequestDto(year * 12, amount, interestRate));
+    var totalPaid3Year = paymentSchedule.PaymentScheduleRows.Take(3 * 12).Sum(w => w.PrincipalAmount);
+    var totalPaidFiveYear = paymentSchedule.PaymentScheduleRows.Take(5 * 12).Sum(w => w.PrincipalAmount);
+    var totalPaid10Year = paymentSchedule.PaymentScheduleRows.Take(10 * 12).Sum(w => w.PrincipalAmount);
+    Console.WriteLine(
+        ($"{year} year, ").PadRight(10) +
+        $"Payment amount:{paymentSchedule.PaymentScheduleRows.First().PaymentAmount:C} " +
+        $"Total Paid:{paymentSchedule.PaymentScheduleRows.Sum(w => w.PaymentAmount):C}, " +
+        $"Total principal at 3 years:{totalPaid3Year:C}, Total at 5 years {totalPaidFiveYear}, Total at 10 years {totalPaid10Year:C}");
+}
+    
+
+static PaymentSchedule CalculateAmortizationSchedule(LoanRequestDto loan)
+{
+    if (loan is null) throw new NullReferenceException($"Parameter {nameof(loan)} cannot be NULL");
+
+    if (loan.LoanAmount <= 0)
+        throw new NullReferenceException($"Invalid loan amount! The loan amount cannot be less than or equal to 0");
+
+    var amortizationSchedule = new List<PaymentScheduleRow>();
+    var monthlyInterestRate = (decimal)(loan.InterestRate / 12.0 / 100.0);
+    var monthlyPayment =
+        Math.Round(CalculateMonthlyPayment(loan.LoanAmount, monthlyInterestRate, loan.NumberOfPayments), 2);
+
+    var remainingBalance = loan.LoanAmount;
+
+    for (var number = 1; number <= loan.NumberOfPayments; number++)
+    {
+        var interestPayment = Math.Round(remainingBalance * monthlyInterestRate, 2);
+        var principalPayment = monthlyPayment - interestPayment;
+
+        remainingBalance -= principalPayment;
+        if (remainingBalance < principalPayment)
+        {
+            principalPayment += remainingBalance;
+            monthlyPayment += remainingBalance;
+            remainingBalance = 0;
+        }
+
+        amortizationSchedule.Add(new PaymentScheduleRow(number, principalPayment, interestPayment, monthlyPayment,
+            remainingBalance));
+    }
+
+    return new PaymentSchedule(loan, amortizationSchedule.Sum(w => w.InterestAmount),
+        amortizationSchedule.ToArray());
+}
+
+static decimal CalculateMonthlyPayment(decimal loanAmount, decimal monthlyInterestRate, int numberOfPayments)
+{
+    if (monthlyInterestRate <= 0)
+        return loanAmount / numberOfPayments;
+
+    var power = (decimal)Math.Pow(1 + (double)monthlyInterestRate, numberOfPayments);
+    return loanAmount * (monthlyInterestRate * power) / (power - 1);
+}
+
+
+internal record LoanRequestDto(int NumberOfPayments, decimal LoanAmount, double InterestRate);
+
+internal record PaymentSchedule(
+    LoanRequestDto LoanRequestDto,
+    decimal TotalInterestRate,
+    PaymentScheduleRow[] PaymentScheduleRows);
+
+internal record PaymentScheduleRow(
+    int PaymentNumber,
+    decimal PrincipalAmount,
+    decimal InterestAmount,
+    decimal PaymentAmount,
+    decimal RemainingBalance);
---
+++ InterestCalculator/Program.vb
@@ -0,0 +1,109 @@
+Imports System
+
+Class LoanRequestDto
+    Sub new(numberOfPayments as Integer, loanAmount as Decimal, interestRate as Double)
+        Me.NumberOfPayments = numberOfPayments
+        Me.LoanAmount = loanAmount
+        Me.InterestRate = interestRate
+    End Sub
+
+    Public ReadOnly Property NumberOfPayments as Integer
+    Public ReadOnly Property LoanAmount as Decimal
+    Public ReadOnly Property InterestRate as Double
+End Class
+
+Class PaymentSchedule
+    Sub new(loanRequestDto as LoanRequestDto,
+            totalInterestPaid as decimal,
+            paymentScheduleRows as PaymentScheduleRow())
+
+        Me.LoanRequestDto = loanRequestDto
+        Me.PaymentScheduleRows = paymentScheduleRows
+        Me.TotalInterestPaid = totalInterestPaid
+    End Sub
+
+    Public ReadOnly Property LoanRequestDto as LoanRequestDto
+    Public ReadOnly Property TotalInterestPaid as Decimal
+    Public ReadOnly Property PaymentScheduleRows as PaymentScheduleRow()
+End Class
+
+Class PaymentScheduleRow
+    Sub new(paymentNumber as Integer,
+            principalAmount as Decimal,
+            interestAmount as Decimal,
+            paymentAmount as Decimal,
+            remainingBalance as Decimal)
+        Me.PaymentNumber = paymentNumber
+        Me.PrincipalAmount = principalAmount
+        Me.InterestAmount = interestAmount
+        Me.PaymentAmount = paymentAmount
+        Me.RemainingBalance = remainingBalance
+    End Sub
+
+    Public Property PaymentNumber As Integer
+    Public Property PrincipalAmount As Decimal
+    Public Property InterestAmount As Decimal
+    Public Property PaymentAmount As Decimal
+    Public Property RemainingBalance As Decimal
+End Class
+
+Module Program
+    private Const Amount as Integer = 200_000
+    private Const InterestRate as double = 4.15
+
+    Sub Main(args As String())
+        Console.WriteLine(
+            $"Years,PaymentAmount,PrincipalAmount,InterestAmount,RemainingBalance,YearTotal, 3 year total,5 year total, 10 year total")
+        For year As Integer = 5 To 30 Step 5
+            dim paymentSchedule = CalculateAmortizationSchedule(new LoanRequestDto(year*12, Amount, InterestRate))
+
+            dim totalPaid3Year = paymentSchedule.PaymentScheduleRows.Take(3*12).Sum(Function(w)  w.PrincipalAmount)
+            dim totalPaidFiveYear = paymentSchedule.PaymentScheduleRows.Take(5*12).Sum(Function(w) w.PrincipalAmount)
+            dim totalPaid10Year = paymentSchedule.PaymentScheduleRows.Take(10*12).Sum(Function(w)  w.PrincipalAmount)
+            Console.WriteLine(
+                ($"{year} year, ").PadRight(10) +
+                $"Payment amount:{paymentSchedule.PaymentScheduleRows.First().PaymentAmount:C} " +
+                $"Total Paid:{paymentSchedule.PaymentScheduleRows.Sum(function(w) w.PaymentAmount):C}, " +
+                $"Total principal at 3 years:{totalPaid3Year:C}, Total at 5 years {totalPaidFiveYear _
+                    }, Total at 10 years {totalPaid10Year:C}")
+        Next
+    End Sub
+
+
+    private Function CalculateAmortizationSchedule(loanRequest as LoanRequestDto) As PaymentSchedule
+
+        dim schedule as List(Of PaymentScheduleRow) = New List(Of PaymentScheduleRow)()
+        dim monthlyInterestRate As decimal = loanRequest.InterestRate/12/100
+        dim monthlyPayment = Math.Round(
+            CalculateMonthlyPayment(loanRequest.LoanAmount, monthlyInterestRate, loanRequest.NumberOfPayments), 2)
+
+        dim remaining = loanRequest.LoanAmount
+
+        For number As Integer = 1 To loanrequest.NumberOfPayments
+            dim interestPayment = Math.Round(remaining*monthlyInterestRate, 2)
+            dim principalPayment = monthlyPayment - interestPayment
+
+            remaining -= principalPayment
+            if (remaining < principalPayment) Then
+
+                principalPayment += remaining
+                monthlyPayment += remaining
+                remaining = 0
+            End If
+
+            schedule.Add(New PaymentScheduleRow(number,principalPayment,interestPayment,monthlyPayment,remaining))
+        Next
+
+        Return New PaymentSchedule(loanRequest, schedule.Sum(Function(e) e.InterestAmount), schedule.ToArray())
+    End Function
+
+    Private Function CalculateMonthlyPayment(loanAmount as Decimal,
+                                             monthlyInterest as Decimal,
+                                             numberOfPayments as Integer) As Decimal
+        if (monthlyInterest <= 0) Then
+            return loanAmount/numberOfPayments
+        End If
+        dim power as Decimal = Math.Pow(1 + CType(monthlyInterest, Double), numberOfPayments)
+        return loanAmount*(monthlyInterest*power)/(power - 1)
+    End Function
+End Module
